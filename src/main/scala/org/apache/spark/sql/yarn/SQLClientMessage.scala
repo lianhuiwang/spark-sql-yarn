a1566d3acbe25c5210958169c790b9b8c7fd2dc7
Lianhui Wang <lianhuiwang09@gmail.com>
2016-07-29 00:48:52 +0800

    add apache license

--- src/main/scala/org/apache/spark/sql/yarn/SQLClientMessage.scala
+++ src/main/scala/org/apache/spark/sql/yarn/SQLClientMessage.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.yarn
 
 import java.nio.ByteBuffer
@@ -7,7 +24,7 @@ import org.apache.spark.sql.JobState._
 import org.apache.spark.util.SerializableBuffer
 
 /**
- * Created by taguswang on 2016/4/8.
+ * Messages that ApplicationMaster send to SQL AppClient.
  */
 
 private[spark] sealed trait SQLClientMessage extends Serializable
